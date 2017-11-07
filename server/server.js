f8caeea988469d4a3473da0cf1720301d094f9c6
meatball <meatball@meatballs-MacBook-Pro.local>
2017-11-07 09:34:20 -0800

    postaman committed

--- server/server.js
+++ server/server.js
@@ -1,57 +1,83 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Fired up on port 3000');
+});
 
 //create mongoose model
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
+// var Todo = mongoose.model('Todo', {
+//   text: {
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   },
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   },
+//   completedAt: {
+//     type: Number,
+//     default: null
+//   }
+// });
+//
+// var User = mongoose.model('User', {
+//   email: {
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   }
+// });
 
 //create collections
-var newTodo = new Todo({
-  text: 'Cook dinner'
-});
-
-var otherTodo = new Todo({
-  text: '  edit this   video    '
-});
-
-var newUser = new User({
-  email: '123abcd@gmail.com'
-});
-
-//save collections to database
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save todo');
-});
-
-newUser.save().then((doc) => {
-  console.log('Saved user', doc);
-}, (e) => {
-  console.log('Unable to save user');
-});
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// var otherTodo = new Todo({
+//   text: '  edit this   video    '
+// });
+//
+// var newUser = new User({
+//   email: '123abcd@gmail.com'
+// });
+//
+// //save collections to database
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log('Saved user', doc);
+// }, (e) => {
+//   console.log('Unable to save user');
+// });
