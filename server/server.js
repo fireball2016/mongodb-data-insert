9d2df4e2e2f63d23513f6feab822a1698f155f58
meatball <meatball@meatballs-MacBook-Pro.local>
2017-12-19 15:10:17 -0800

    login route added

--- server/server.js
+++ server/server.js
@@ -119,6 +119,19 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+// POST / user login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.fineByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 // App runs on port
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
